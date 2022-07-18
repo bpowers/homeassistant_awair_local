31f9fd830271c7d6d79ba3ea0bcf73dbac4e594a
Zach Berger <zach@zberger.com>
2022-07-17 20:05:09 -0700

    Rebase code against upstream.
    The only differences compared to the code home-assistant/core
    and this repository are what are needed to make this work locally.

--- __init__.py
+++ __init__.py
@@ -1,57 +1,44 @@
 """The awair component."""
+from __future__ import annotations
 
 from asyncio import gather
-from typing import Any, Optional
+from typing import Any
 
 from async_timeout import timeout
 from python_awair import AwairLocal
 
-from homeassistant.const import CONF_HOSTS
-from homeassistant.core import Config, HomeAssistant
-from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_HOSTS, Platform
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-from .const import API_LOCAL_TIMEOUT, DOMAIN, LOGGER, UPDATE_INTERVAL, AwairResult
+from .const import API_TIMEOUT, DOMAIN, LOGGER, UPDATE_INTERVAL, AwairResult
 
-PLATFORMS = ["sensor"]
-
-
-async def async_setup(hass: HomeAssistant, config: Config) -> bool:
-    """Set up Awair integration."""
-    return True
+PLATFORMS = [Platform.SENSOR]
 
 
-async def async_setup_entry(hass, config_entry) -> bool:
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Set up Awair integration from a config entry."""
     session = async_get_clientsession(hass)
     coordinator = AwairDataUpdateCoordinator(hass, config_entry, session)
 
-    await coordinator.async_refresh()
-
-    if not coordinator.last_update_success:
-        raise ConfigEntryNotReady
+    await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][config_entry.entry_id] = coordinator
 
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(config_entry, platform)
-        )
+    hass.config_entries.async_setup_platforms(config_entry, PLATFORMS)
 
     return True
 
 
-async def async_unload_entry(hass, config_entry) -> bool:
+async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Unload Awair configuration."""
-    tasks = []
-    for platform in PLATFORMS:
-        tasks.append(
-            hass.config_entries.async_forward_entry_unload(config_entry, platform)
-        )
+    unload_ok = await hass.config_entries.async_unload_platforms(
+        config_entry, PLATFORMS
+    )
 
-    unload_ok = all(await gather(*tasks))
     if unload_ok:
         hass.data[DOMAIN].pop(config_entry.entry_id)
 
@@ -60,8 +47,6 @@ async def async_unload_entry(hass, config_entry) -> bool:
 
 class AwairDataUpdateCoordinator(DataUpdateCoordinator):
     """Define a wrapper class to update Awair data."""
-
-    _awair: AwairLocal
 
     def __init__(self, hass, config_entry, session) -> None:
         """Set up the AwairDataUpdateCoordinator class."""
@@ -72,18 +57,18 @@ class AwairDataUpdateCoordinator(DataUpdateCoordinator):
 
         super().__init__(hass, LOGGER, name=DOMAIN, update_interval=UPDATE_INTERVAL)
 
-    async def _async_update_data(self) -> Optional[Any]:
+    async def _async_update_data(self) -> Any | None:
         """Update data via Awair client library."""
-        with timeout(API_LOCAL_TIMEOUT):
+        async with timeout(API_TIMEOUT):
             try:
                 LOGGER.debug("Fetching devices")
                 devices = await self._awair.devices()
                 results = await gather(
-                    *[self._fetch_air_data(device) for device in devices]
+                    *(self._fetch_air_data(device) for device in devices)
                 )
                 return {result.device.uuid: result for result in results}
             except Exception as err:
-                raise UpdateFailed(err)
+                raise UpdateFailed(err) from err
 
     async def _fetch_air_data(self, device):
         """Fetch latest air quality data."""
